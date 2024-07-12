b70518aca9451d922e99d30ef4a4fbb30dc51213
Bonevy BEBY <bonevybeby@gmail.com>
2024-07-12 04:10:05 +0200

    added: mutableState and Log.i

--- app/src/main/java/com/formationkilo/mylogin/LoginScreen.kt
+++ app/src/main/java/com/formationkilo/mylogin/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.formationkilo.mylogin
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -17,16 +18,27 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun LoginScreen(){
+  var email by remember {
+    mutableStateOf("")
+  }
+
+  var password by remember {
+    mutableStateOf("")
+  }
   Column(modifier = Modifier.fillMaxSize(),
     verticalArrangement = Arrangement.Center,
     horizontalAlignment = Alignment.CenterHorizontally) {
@@ -38,16 +50,22 @@ fun LoginScreen(){
     Text(text = "Login to your account")
     //TextField(value = "h,t", onValueChange = {})
     Spacer(modifier = Modifier.height(16.dp))
-    OutlinedTextField(value = "", onValueChange ={}, label = {
+    OutlinedTextField(value = email, onValueChange ={
+      email=it
+    }, label = {
       Text(text = "Email address")
     } )
 
     Spacer(modifier = Modifier.height(16.dp))
-    OutlinedTextField(value = "", onValueChange ={}, label = {
+    OutlinedTextField(value = password, onValueChange ={
+      password=it
+    }, label = {
       Text(text = "Password")
-    } )
+    }, visualTransformation = PasswordVisualTransformation() )
     Spacer(modifier = Modifier.height(16.dp))
-    Button(onClick = {  }) {
+    Button(onClick = {
+      Log.i("Credential"," Email: $email, Password: $password")
+    }) {
       Text(text = "Login")
       
     }
@@ -62,26 +80,34 @@ fun LoginScreen(){
 
     Text(text = "Or sign in with ")
    Row(
-     modifier = Modifier.fillMaxWidth().padding(40.dp),
+     modifier = Modifier
+       .fillMaxWidth()
+       .padding(40.dp),
      horizontalArrangement = Arrangement.SpaceEvenly
    ) {
      Image(painter = painterResource(id = R.drawable.facebook_logo_png) ,
        contentDescription ="Facebook",
-       modifier = Modifier.size(60.dp).clickable {
-         //Facebook clicked
-       })
+       modifier = Modifier
+         .size(60.dp)
+         .clickable {
+           //Facebook clicked
+         })
 
      Image(painter = painterResource(id = R.drawable.google) ,
        contentDescription ="Google",
-       modifier = Modifier.size(60.dp).clickable {
-         //Facebook clicked
-       })
+       modifier = Modifier
+         .size(60.dp)
+         .clickable {
+           //Facebook clicked
+         })
 
      Image(painter = painterResource(id = R.drawable.x_social_media) ,
        contentDescription ="Twitter",
-       modifier = Modifier.size(60.dp).clickable {
-         //Facebook clicked
-       })
+       modifier = Modifier
+         .size(60.dp)
+         .clickable {
+           //Facebook clicked
+         })
    }
   }
 
